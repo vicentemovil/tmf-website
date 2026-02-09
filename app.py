5f7637481a69e551a955f5834da931fb49c8f48c
Vicente Alvarez <vicente@Vicentes-MacBook-Air.local>
2026-02-09 14:50:48 +0200

    app

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
+
+@app.before_request
+def redirect_www():
+    host = request.host
+    if host.startswith("www."):
+        return redirect(
+            request.url.replace(host, host[4:], 1),
+            code=301
+        )
 
 
 @app.route("/")
